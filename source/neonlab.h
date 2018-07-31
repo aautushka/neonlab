0700cdc8402df8597744e7e74f9e45fe400c7e74
Anton Autushka <aautushka@wanna.by>
2018-07-31 19:20:33 +0300

    working on zero test

--- source/neonlab.h
+++ source/neonlab.h
@@ -1,1 +1,39 @@
-const char* hello() { return "hello world";}
+#include <cassert>
+
+const void* memnz_naive(const void* x, int len) {
+    auto p = (uint8_t*)x;
+    auto end = p + len;
+
+    while (p < end && *p) {
+        ++p;
+    }
+
+    return p == end ? nullptr : p;
+    ;
+}
+
+uint32_t testnz(const void* x, int len) {
+    assert(((uint64_t)x & 0xf) == 0);
+    uint32_t result;
+    asm volatile(R"(
+        mov r2, #16
+        vld1.8 {q0}, [%1]
+        add %1, #16
+        vld1.8 {q1}, [%1]
+        vorr q2, q0, q1
+        vorr d0, d4, d5
+        vmov.32 r0, d0[0]
+        vmov.32 r1, d0[1]
+        orr %0, r0, r1
+    )"
+                 : "=r"(result)
+                 : "r"(x), "r"(len));
+    return result;
+}
+
+uint32_t testz(const void* x, int len) { return !testnz(x, len); }
+
+const void* memnz_neon(const void* x, int len) {
+    const void* result;
+    return result;
+}
--- tests/neonlab_tests.cpp
+++ tests/neonlab_tests.cpp
@@ -1,12 +1,48 @@
 #include <gtest/gtest.h>
 #include "neonlab.h"
 
-struct neonlab_test : ::testing::Test
-{
+template <int N>
+struct aligned_buf {
+    uint8_t raw[2 * N];
+    uint8_t* get() { return (uint8_t*)(((((uint64_t)raw - 1) >> 4) + 1) << 4); }
+    uint8_t& operator[](int i) { return *(get() + i); }
+    void set_zero() { memset(get(), 0, N); }
 };
 
-TEST_F(neonlab_test, hello_world)
-{
-    EXPECT_STREQ("hello world", hello());
-}
+struct neonlab_test : ::testing::Test {};
 
+TEST_F(neonlab_test, contains_all_zeros) {
+    aligned_buf<32> buf{};
+    EXPECT_TRUE(testz(buf.get(), 32));
+};
+
+TEST_F(neonlab_test, not_all_zeros) {
+    aligned_buf<32> buf{};
+    buf[4] = 1;
+    EXPECT_FALSE(testz(buf.get(), 32));
+
+    buf[0] = 0;
+    buf[7] = 1;
+    EXPECT_FALSE(testz(buf.get(), 32));
+
+    buf[7] = 0;
+    buf[15] = 1;
+    EXPECT_FALSE(testz(buf.get(), 32));
+
+    buf[15] = 0;
+    buf[23] = 1;
+    EXPECT_FALSE(testz(buf.get(), 32));
+
+    buf[23] = 0;
+    buf[31] = 1;
+    EXPECT_FALSE(testz(buf.get(), 32));
+};
+
+TEST_F(neonlab_test, comprehensive_nonzero_test) {
+    aligned_buf<32> buf{};
+    for (int i = 0; i < 32; ++i) {
+        buf.set_zero();
+        buf[i] = 0xff;
+        EXPECT_FALSE(testz(buf.get(), 32));
+    }
+}
